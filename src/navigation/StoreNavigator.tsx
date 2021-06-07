b9b541f184ed5dd6d2b3b92d0e9d63e10583c93b
LigaData-RahmaA <rahmeh@legadata.com>
2021-06-07 16:21:36 +0300

    DOK-59/ Drawer

--- src/navigation/StoreNavigator.tsx
+++ src/navigation/StoreNavigator.tsx
@@ -1,14 +1,20 @@
 import {createStackNavigator} from '@react-navigation/stack'
 import React from 'react'
 import AddProductScreen from '../screens/product/AddProductScreen'
+import {createDrawerNavigator} from '@react-navigation/drawer'
+const Drawer = createDrawerNavigator()
 
 const Stack = createStackNavigator()
 
 const StoreNavigator = () => {
   return (
-    <Stack.Navigator headerMode='none'>
+    <Drawer.Navigator
+    // drawerType={dimensions.width >= 768 ? 'permanent' : 'front'}
+    >
+      {/* <Stack.Navigator headerMode='none'> */}
       <Stack.Screen name='AddProductScreen' component={AddProductScreen} />
-    </Stack.Navigator>
+      {/* </Stack.Navigator> */}
+    </Drawer.Navigator>
   )
 }
 
